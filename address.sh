192654d42bdcbddf50772fd0246d6c35e6ae984a
RishabhB99 <46969858+RishabhB99@users.noreply.github.com>
2019-08-19 16:16:00 +0530

    Add files via upload

--- address.sh
+++ address.sh
@@ -10,6 +10,8 @@ then
 	file="player.txt"
 	l=0
 	flag=0
+	first=$(echo $name|awk '{print $1}')
+	second=$(echo $name|awk '{print $2}')
 	while IFS= read -r line
 	do
 		a=$(echo $line|awk '{print $1}')
@@ -17,10 +19,13 @@ then
 		c=$(echo $line|awk '{print $3}')
 		
 		l=$((l+1))
-		if [[ $a == "$name" ]]
+		if [[ $a == "$first" ]]
 		then
-			flag=$((flag+1))
-			break
+			if [[ $b == "$second" ]]
+			then
+				flag=$((flag+1))
+				break
+			fi	
 		fi
 	done<"$file"		
 	
@@ -29,13 +34,17 @@ then
 		read -p 'Want to replace[YES/NO]: ' tem
 		if [[ $tem == "YES" ]]
 		then
-			sed -i /$a/d player.txt
+			sed -i /"$name"/d player.txt
 			printf "%s %s %s \n" "$name $team $avg" >> player.txt
 		fi	
 	else	
 		printf "%s %s %s \n" "$name $team $avg" >> player.txt
 	fi	
-	column -t player.txt 
+
+	sort -k 4n player.txt > t1.txt
+	tac t1.txt > player.txt
+
+	column -t player.txt
 fi	
 
 if [ $var -eq 2 ]
@@ -50,14 +59,23 @@ then
 		a=$(echo $line|awk '{print $1}')
 		b=$(echo $line|awk '{print $2}')
 		c=$(echo $line|awk '{print $3}')
-
+		d="$a $b"
 		if [[ $a == "$str" ]]
 		then
 			echo $line >> search.txt
-			break
 		fi	
 
 		if [[ $b == "$str" ]]
+		then
+			echo $line >> search.txt
+		fi
+
+		if [[ $c == "$str" ]]
+		then
+			echo $line >> search.txt
+		fi				
+		
+		if [[ $d == "$str" ]]
 		then
 			echo $line >> search.txt
 		fi
@@ -67,7 +85,10 @@ then
 	if [[ $rem == "YES" ]]
 	then
 		sed -i /$str/d player.txt
-	fi	
+	fi
+
+	sort -k 4n player.txt > t1.txt
+	tac t1.txt > player.txt
 fi			
 if [ $var -eq 4 ]
 then
@@ -87,13 +108,15 @@ then
 	do
 		a=$(echo $line|awk '{print $1}')
 		b=$(echo $line|awk '{print $2}')
-		n="$a$b"
 		c=$(echo $line|awk '{print $9}')
 		team="IND"
-		echo "$n $team $c " >> player.txt
+		echo "$a $b $team $c " >> player.txt
 	done<"$file"
+
+	sort -k 4n player.txt > t1.txt
+	tac t1.txt > player.txt
 	column -t player.txt
 fi
 
-./address.sh
+ ./address.sh
 
